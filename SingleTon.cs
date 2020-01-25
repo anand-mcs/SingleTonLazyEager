43c631012f674d84149d10be1f88231cb9988b7e
anand-mcs <59893145+anand-mcs@users.noreply.github.com>
2020-01-25 18:22:47 +0530

    First Commit

---
+++ SingleTon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Anand Jayaraman
+namespace SingleTonPattern
+{
+    public sealed class SingleTon
+    {
+        private static int counter;
+        //private static readonly object obj = new object();
+        //Changed the constructor to private to control the instantiation.
+        private static SingleTon Instance = new SingleTon();
+        //private static readonly Lazy<SingleTon> Instance = new Lazy<SingleTon>(()=>new SingleTon());
+        public int SingleTonValue = 0;
+
+        public static SingleTon GetInstance
+        {
+            get
+            {
+
+
+                //   if (Instance == null)
+                //    Instance = new SingleTon();
+
+                //return Instance.Value;
+                return Instance;
+
+            }
+        }
+       
+        
+    private SingleTon()
+        {
+            counter++;
+            SingleTonValue = 0;
+            Console.WriteLine("Shopping Cart Single Instance kicked off " + counter.ToString());
+    
+        }
+        public void Display(string Msg)
+        {
+            Console.WriteLine(Msg);
+        }
+    }
+}
